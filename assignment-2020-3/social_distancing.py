e85ebdce4e2d5b07ed5970358bdf5fde13d5a529
Natalia Katsiapi <kat26natalia@gmail.com>
2020-05-14 01:33:02 +0300

    Make basic one

--- assignment-2020-3/social_distancing.py
+++ assignment-2020-3/social_distancing.py
@@ -1,5 +1,6 @@
 import math
 
+
 def find_tangent_circle(cm, cn, r):
     dx = cn[0] - cm[0]
     dy = cn[1] - cm[1]
@@ -8,26 +9,34 @@ def find_tangent_circle(cm, cn, r):
     r2 = cn[2] + r
     l = (r1 ** 2 - r2 ** 2 + d ** 2) / (2 * d ** 2)
     e = math.sqrt(r1 ** 2 / d ** 2 - l ** 2)
-    c = (round(cm[0] + l * dx - e * dy, 2), round(cm[1] + l * dy + e * dx, 2), r)
+    c = (round(cm[0] + l * dx - e * dy, 2),
+         round(cm[1] + l * dy + e * dx, 2), r)
     return c
+
 
 def get_previous(d, element):
     return list(d[element])[0]
 
+
 def get_next(d, element):
     return list(d[element])[1]
+
 
 def update_previous(battlefront, element, value):
     battlefront[element][0] = value
 
+
 def update_next(battlefront, element, value):
     battlefront[element][1] = value
+
 
 def find_circle_distance_from_00(c):
     return round(math.sqrt(c[0] ** 2 + c[1] ** 2) - c[2], 2)
 
+
 def get_circle_nearest_to_beginning(circles):
     return min(circles, key=lambda k: k[1])
+
 
 def do_circles_intersect(c1, c2):
     dist = round(math.sqrt((c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2), 2)
@@ -35,12 +44,52 @@ def do_circles_intersect(c1, c2):
         return True
     return False
 
-def does_circle_intersect_with_battlefront(battlefront, ci):
+
+def get_intersecting_circle(battlefront, ci, cm):
+    circles = set()
     for c in battlefront:
         if do_circles_intersect(ci, c):
-            # print(c)
-            return True
-    return False
+            circles.add(c)
+    if len(circles) != 0:
+        return min(circles, key=lambda k: math.sqrt((cm[0] - k[0]) ** 2 + (cm[1] - k[1]) ** 2))
+    return 0
+
+
+def delete_from_battlefront_dict(battlefront, key):
+    previous = get_previous(battlefront, key)
+    next = get_next(battlefront, key)
+    del battlefront[key]
+    update_next(battlefront, previous, next)
+    update_previous(battlefront, next, previous)
+    return battlefront
+
+
+def step5(battlefront, ci, cm, cn, cj, battlefront_in_order):
+    semicircle = len(battlefront) // 2
+    in_semicircle = False
+    current = cm
+    for i in range(semicircle):
+        current = get_previous(battlefront, current)
+        if current == ci:
+            in_semicircle = True
+            break
+    if in_semicircle:
+        a = get_next(battlefront, cj)
+        while a != cn:
+            print(a)
+            x = get_next(battlefront, a)
+            battlefront = delete_from_battlefront_dict(battlefront, a)
+            # battlefront_in_order.remove(a)
+            a = x
+        return 1
+    else:
+        a = get_previous(battlefront, cj)
+        while a != cn:
+            x = get_previous(battlefront, a)
+            battlefront = delete_from_battlefront_dict(battlefront, a)
+            # battlefront_in_order.remove(a)
+            a = x
+    return 2
 
 def write_in_file(circles, filename):
     f = open(filename, 'w')
@@ -49,32 +98,36 @@ def write_in_file(circles, filename):
         f.write(line)
     f.close()
 
+
 r = 2
 c1 = (0, 0, r)
 c2 = (0, 2 * r, r)
-battlefront = {c1 : [c2, c2], 
-                c2 : [c1, c1]}
-battlefront_in_order = [(c1, find_circle_distance_from_00(c1)), (c2, find_circle_distance_from_00(c2))]
+battlefront = {c1: [c2, c2],
+               c2: [c1, c1]
+               }
+battlefront_in_order = [(c1, find_circle_distance_from_00(
+    c1)), (c2, find_circle_distance_from_00(c2))]
 circles = [c1, c2]
 print(battlefront)
-for i in range(5):
-    cm = get_circle_nearest_to_beginning(battlefront_in_order)[0]
-    cn = get_next(battlefront, cm) 
-    print(cn)
+cm = get_circle_nearest_to_beginning(battlefront_in_order)[0]
+for i in range(150):
+    cn = get_next(battlefront, cm)
     ci = find_tangent_circle(cm, cn, r)
-    print(cm, cn, ci)
-    print(battlefront)
-    print(does_circle_intersect_with_battlefront(battlefront, ci))
-    if not does_circle_intersect_with_battlefront(battlefront, ci):
+    inters = get_intersecting_circle(battlefront, ci, cm)
+    if inters == 0:
         battlefront[ci] = [cm, cn]
         update_previous(battlefront, cn, ci)
         update_next(battlefront, cm, ci)
-        battlefront_in_order.append((ci, find_circle_distance_from_00(ci)))    
-        # print(battlefront)
-
+        battlefront_in_order.append((ci, find_circle_distance_from_00(ci)))
         circles.append(ci)
-    for c in battlefront:
-        if get_next(battlefront, c) == cm:
-            ci = c
-    
+    else:
+        if step5(battlefront, ci, cm, cn, inters, battlefront_in_order) == 1:
+            cm = inters
+        else:
+            cn = inters
+
+    # for c in battlefront:
+    #     if get_next(battlefront, c) == cm:
+    #         ci = c
+
 write_in_file(circles, "output_file")
